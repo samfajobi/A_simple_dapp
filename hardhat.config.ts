5d10986001e5c70c1c7f3602cd5baacfb22ef731
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-08-02 00:43:52 +0100

    update Config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,26 @@
 //import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+const {ALCHEMY_API_KEY, PRIVATE_KEY} = process.env;
+
 module.exports = {
   defaultNetwork: "sepolia",
   networks: {
     hardhat: {
     },
     sepolia: {
-      url: "https://sepolia.infura.io/v3/<key>",
-      accounts: [privateKey1, privateKey2, ...]
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
     }
   },
   solidity: {
     version: "0.8.19",
-    settings: {
+    settings: { 
       optimizer: {
         enabled: true,
         runs: 200
       }
     }
   },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-  mocha: {
-    timeout: 40000
-  }
 }
