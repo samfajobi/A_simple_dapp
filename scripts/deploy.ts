ba5d8e111d57f54245f1c03968ffb7e7df089966
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-08-02 01:48:40 +0100

    Contract deployed successfully

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,11 +5,16 @@ async function main() {
   const Transactions = await hre.ethers.getContractFactory("Transaction")
   const transactions = await Transactions.deploy();
 
-  await transactions.deployed();
 
-  console.log("Your transaction has been deployed to:", transactions.address)
+  //await transactions.deployed();
 
-  
+  await transactions.waitForDeployment();
+
+ 
+  const address = await transactions.getAddress();
+  console.log(`Your transaction has been deployed to: ${address}`)
+
+  //return transactions;
 }
 
 // We recommend this pattern to be able to use async/await everywhere
